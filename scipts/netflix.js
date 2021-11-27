dfeb33ed4c7a1dc5167b36e3df7a35b9b69c765d
KCLouw <klouw07@gmail.com>
2021-11-27 21:28:15 +0200

    added a feature to toggle between the 'movie' state and 'watchlist' state

--- scipts/netflix.js
+++ scipts/netflix.js
@@ -1,17 +1,38 @@
+
+
 let myVue = new Vue({
     el: '#myApp',
 
     data: {
-        watchlist: []
+        watchlist: [],
+        state: 'no-watchlist'
     },
 
     methods: {
         addMovie(x) {
-            this.watchlist.push(x)
+           return this.watchlist.push(x);
         },
 
         dltMovie() {
             this.watchlist.pop()
+        },
+        save() {
+            let myMovie = JSON.stringify(watchlist['title']);
+            localStorage.setItem('userInput', myMovie);
+
+            let savedMovie = localStorage.getItem('userInput');
+            let returnedMovie = JSON.parse(savedMovie); 
+        },
+
+        changeState(newState) {
+            this.state = newState;
         }
     },
 })
+            
+            // return document.getElementById('savedList').innerHTML = returnedMovie;
+// let myMovie = JSON.stringify(watchlist);
+// localStorage.setItem('userInput', watchlist);
+
+// let savedMovie = localStorage.getItem('userInput');
+// let newMovie = JSON.parse(savedMovie);
